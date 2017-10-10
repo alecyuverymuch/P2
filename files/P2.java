d5642ee8cc40eb1d862290277a2670aa90080bf5
alecyuverymuch <amyu@wisc.edu>
2017-10-10 14:16:53 -0500

    added tests

--- files/P2.java
+++ files/P2.java
@@ -20,6 +20,9 @@ public class P2 {
         CharNum.num = 1;
 
         testComments();
+        CharNum.num = 1;
+        
+        testValidTokens();
     }
 
     /**
@@ -376,4 +379,401 @@ public class P2 {
             outFile.close();
         }
     }
+
+    private static void testValidTokens() throws IOException {
+        // open input and output files
+            FileReader inFile = null;
+            PrintWriter outFile = null;
+            try {
+                inFile = new FileReader("testTokens.in");
+                outFile = new PrintWriter(new FileWriter("testTokens.out"));
+            } catch (FileNotFoundException ex) {
+                System.err.println("File comment.in not found.");
+                System.exit(-1);
+            } catch (IOException ex) {
+                System.err.println("comment.out cannot be opened.");
+                System.exit(-1);
+            }
+            // create and call the scanner
+            Yylex scanner = new Yylex(inFile);
+            
+        
+        int lineNum = 1;
+        //String tests 
+        int numStrCases = 6; //this is the number of string cases it checks
+        for(int i = 0;i<numStrCases;i++){
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.STRINGLITERAL){
+            outFile.println("Error expected token STRINGLITERAL but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected String Literal on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            lineNum++;
+        }
+        
+        //bool tests
+        int numBoolCases = 13; //this is the number of bool cases it checks
+        for(int i = 0;i<numBoolCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.BOOL){
+            outFile.println("Error expected token BOOL but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected BOOL on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //int tests
+        int numIntCases = 13;
+        for(int i = 0;i<numIntCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.INT){
+            outFile.println("Error expected token INT but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected INT on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //void tests
+        int numVoidCases = 13;
+        for(int i = 0;i<numVoidCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.VOID){
+            outFile.println("Error expected token VOID but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected VOID on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //true tests
+        int numTrueCases = 13;
+        for(int i = 0;i<numTrueCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.TRUE){
+            outFile.println("Error expected token TRUE but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected TRUE on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //false tests
+        int numFalseCases = 13;
+        for(int i = 0;i<numFalseCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.FALSE){
+            outFile.println("Error expected token FALSE but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected FALSE on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //struct tests
+        int numStructCases = 13;
+        for(int i = 0;i<numStructCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.STRUCT){
+            outFile.println("Error expected token Struct but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected Struct on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+             //cin tests
+            int numCinCases = 13;
+        for(int i = 0;i<numCinCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.CIN){
+            outFile.println("Error expected token CIN but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected CIN on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+            //cout tests
+            int numCoutCases = 13;
+        for(int i = 0;i<numCoutCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.COUT){
+            outFile.println("Error expected token COUT but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected COUT on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //if tests
+        int numIfCases = 13;
+        for(int i = 0;i<numIfCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.IF){
+            outFile.println("Error expected token IF but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected IF on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //else tests
+        int numElseCases = 13;
+        for(int i = 0;i<numElseCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.ELSE){
+            outFile.println("Error expected token ELSE but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected ELSE on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //while
+        int numWhileCases = 13;
+        for(int i = 0;i<numWhileCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.WHILE){
+            outFile.println("Error expected token WHILE but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected WHILE on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //return tests
+        int numReturnCases = 13;
+        for(int i = 0;i<numReturnCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.RETURN){
+            outFile.println("Error expected token RETURN but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected RETURN on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        int numIdCases = 24;
+        for(int i = 0;i<numIdCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.ID){
+            outFile.println("Error expected token ID but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected ID on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        int numIntLitCases = 13;
+        for(int i = 0;i<numIntLitCases;i++){
+            scanner.next_token();
+            Symbol token = scanner.next_token();
+            if(token.sym == sym.EOF){
+            outFile.println("EOF reached one or more tokens might have been found invalid");
+            break;
+            }
+            if(token.sym != sym.INTLITERAL){
+            outFile.println("Error expected token INTLITERAL but found " + token.sym +
+                    " on line " + lineNum);
+            } 
+            if(((TokenVal)token.value).linenum != lineNum){
+            outFile.println("Error expected INTLITERAL on line " + lineNum +
+                    " but had line num... " + lineNum);
+            }
+            scanner.next_token();
+            lineNum++;
+        }
+        //tests symbols / charNums
+        int charnum = 1;
+        check(sym.LCURLY, charnum++,scanner.next_token(),"LCURLY",outFile);
+        check(sym.RCURLY, charnum++,scanner.next_token(),"RCURLY",outFile);
+        check(sym.LPAREN, charnum++,scanner.next_token(),"LPAREN",outFile);
+        check(sym.RPAREN, charnum++,scanner.next_token(),"RPAREN",outFile);
+        check(sym.SEMICOLON, charnum++,scanner.next_token(),"SEMICOLON",outFile);
+        check(sym.COMMA, charnum++,scanner.next_token(),"COMMA",outFile);
+        check(sym.DOT, charnum++,scanner.next_token(),"DOT",outFile); 
+        check(sym.MINUS, charnum++,scanner.next_token(),"MINUS",outFile);
+        check(sym.TIMES, charnum++,scanner.next_token(),"TIMES",outFile);
+        check(sym.DIVIDE, charnum++,scanner.next_token(),"DIVIDE",outFile);
+        check(sym.NOT, charnum++,scanner.next_token(),"NOT",outFile);
+        check(sym.LESS, charnum++,scanner.next_token(),"LESS THAN",outFile);
+        check(sym.GREATER, charnum++,scanner.next_token(),"GREATER THAN",outFile);
+        check(sym.WRITE, charnum,scanner.next_token(),"COUT",outFile);
+        charnum += 2;
+        check(sym.READ, charnum,scanner.next_token(),"CIN",outFile);
+        charnum += 2;
+        check(sym.PLUSPLUS, charnum,scanner.next_token(),"PLUSPLUS",outFile);
+        charnum += 2;
+        check(sym.AND, charnum,scanner.next_token(),"AND",outFile);
+        charnum += 2;
+        check(sym.OR, charnum,scanner.next_token(),"OR",outFile);
+        charnum += 2;
+        check(sym.EQUALS, charnum,scanner.next_token(),"EQUALS",outFile);
+        charnum += 2;
+        check(sym.NOTEQUALS, charnum,scanner.next_token(),"NOTEQUALS",outFile);
+        charnum += 2;
+        check(sym.LESSEQ, charnum,scanner.next_token(),"LESSEQ",outFile);
+        charnum += 2;
+        check(sym.GREATEREQ, charnum,scanner.next_token(),"GREATEREQ",outFile);
+        charnum += 2;
+        check(sym.BOOL, charnum,scanner.next_token(),"BOOL",outFile);
+        charnum += 5;
+        check(sym.INT, charnum,scanner.next_token(),"INT",outFile);
+        charnum += 4;
+        check(sym.VOID, charnum,scanner.next_token(),"VOID",outFile);
+        charnum += 5;
+        check(sym.TRUE, charnum,scanner.next_token(),"TRUE",outFile);
+        charnum += 5;
+        check(sym.FALSE, charnum,scanner.next_token(),"FALSE",outFile);
+        charnum += 6;
+        check(sym.STRUCT, charnum,scanner.next_token(),"STRUCT",outFile);
+        charnum += 7;
+        check(sym.CIN, charnum,scanner.next_token(),"CIN",outFile);
+        charnum += 4;
+        check(sym.COUT, charnum,scanner.next_token(),"COUT",outFile);
+        charnum += 5;
+        check(sym.IF, charnum,scanner.next_token(),"IF",outFile);
+        charnum += 3;
+        check(sym.ELSE, charnum,scanner.next_token(),"ELSE",outFile);
+        charnum += 5;
+        check(sym.WHILE, charnum,scanner.next_token(),"WHILE",outFile);
+        charnum += 6;
+        check(sym.RETURN, charnum,scanner.next_token(),"RETURN",outFile);
+        charnum += 7;
+        check(sym.ID, charnum,scanner.next_token(),"ID",outFile);
+        charnum += 2;
+        check(sym.INTLITERAL, charnum,scanner.next_token(),"INTLIT",outFile);
+        charnum += 2;
+        check(sym.STRINGLITERAL, charnum,scanner.next_token(),"STRLIT",outFile);
+        charnum += 3;
+            outFile.close();
+        }
+    
+        private static void check(int s,int charNum, Symbol token,String tokenName,PrintWriter out){
+        if(token.sym == 0){
+            out.println("EOF reached too early on last line token may have been missed "
+                + tokenName);
+            return;
+            }
+        if(token.sym != s){
+            out.println("Error expected "+ tokenName + " but found " + token.sym + 
+                " on last line");
+        }
+        if(((TokenVal)token.value).charnum != charNum){
+            out.println("Error expected char num " + charNum + " for token " + tokenName);
+        }
+        }
 }
